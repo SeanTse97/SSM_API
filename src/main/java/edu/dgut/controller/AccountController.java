b41beb02c89e5f4105f5c31662498a1b7bcae113
super_xin <m13423197429@163.com>
2020-06-10 15:18:29 +0800

    用户登陆控制

--- src/main/java/edu/dgut/controller/AccountController.java
+++ src/main/java/edu/dgut/controller/AccountController.java
@@ -1,38 +1,61 @@
 package edu.dgut.controller;
 
-import edu.dgut.domain.Account;
+import com.alibaba.fastjson.JSON;
+import com.alibaba.fastjson.JSONArray;
+import edu.dgut.domain.User;
+import edu.dgut.domain.User2;
 import edu.dgut.service.AccountService;
+import edu.dgut.util.JsonUtil;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
-import org.springframework.web.servlet.ModelAndView;
 
-import java.util.List;
+import java.util.HashMap;
 
 @Controller
-@RequestMapping("/account")
+@RequestMapping("/api")
 public class AccountController {
 
     @Autowired
     AccountService accountService;
 
 
-//    @PostMapping("/list")
-//    @ResponseBody
-//    public List<Account> findAllAccount(){
-//        return accountService.findAll();
-//    }
-    @GetMapping("/list")
-    public ModelAndView jsp(){
-        List<Account> list = accountService.findAll();
-        ModelAndView mv = new ModelAndView();
-        mv.addObject("list",list);
-        mv.setViewName("hello");
-        return mv;
+    @PostMapping("/login")
+    @ResponseBody
+    public HashMap login(User userInfo){
+        HashMap map = new HashMap();
+        map.put("success",Boolean.TRUE);
+        map.put("msg","服务端出错了");
+        return map;
+    }
+    @PostMapping("/register")
+    @ResponseBody
+    public HashMap register(User userInfo){
+        System.out.println(userInfo);
+        HashMap map = new HashMap();
+        map.put("success",Boolean.TRUE);
+        return map;
     }
 
+    @PostMapping("/province/list")
+    @ResponseBody
+    public JSONArray getAllProvince(){
+        String path = AccountController.class.getClassLoader().getResource("map.json").getPath();;
+        String s = JsonUtil.readJsonFile(path);
+        JSONArray jarr = JSON.parseArray(s);
+        return jarr;
+
+    }
+
+    @PostMapping("/updateUserInfo")
+    @ResponseBody
+    public HashMap updateUserInfo(User2 user){
+        System.out.println(user.toString());
+        HashMap map = new HashMap();
+        map.put("success",Boolean.TRUE);
+        return map;
+    }
 
 }
