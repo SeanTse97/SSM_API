8f3ddc25df8b0ca257b6b0f79695231edd18f48b
super_xin <m13423197429@163.com>
2020-06-18 16:27:26 +0800

    添加图片字段

--- src/main/java/edu/dgut/domain/Mask.java
+++ src/main/java/edu/dgut/domain/Mask.java
@@ -7,14 +7,15 @@ public class Mask {
     long editTime;
     String orderId; //入库id
     String maskName;
-    String maskNo;//口罩数量
+    int maskNo;//口罩数量
+    String imageUrl;
 
 
 
     public Mask() {
     }
 
-    public Mask(String addUser, String editUser, long addTime, long editTime, String orderId, String maskName, String maskNo) {
+    public Mask(String addUser, String editUser, long addTime, long editTime, String orderId, String maskName, int maskNo, String imageUrl) {
         this.addUser = addUser;
         this.editUser = editUser;
         this.addTime = addTime;
@@ -22,7 +23,7 @@ public class Mask {
         this.orderId = orderId;
         this.maskName = maskName;
         this.maskNo = maskNo;
-
+        this.imageUrl = imageUrl;
     }
 
     public String getAddUser() {
@@ -73,25 +74,33 @@ public class Mask {
         this.maskName = maskName;
     }
 
-    public String getMaskNo() {
+    public int getMaskNo() {
         return maskNo;
     }
 
-    public void setMaskNo(String maskNo) {
+    public void setMaskNo(int maskNo) {
         this.maskNo = maskNo;
     }
 
+    public String getImageUrl() {
+        return imageUrl;
+    }
+
+    public void setImageUrl(String imageUrl) {
+        this.imageUrl = imageUrl;
+    }
 
     @Override
     public String toString() {
         return "Mask{" +
                 "addUser='" + addUser + '\'' +
                 ", editUser='" + editUser + '\'' +
-                ", addTime='" + addTime + '\'' +
-                ", editTime='" + editTime + '\'' +
+                ", addTime=" + addTime +
+                ", editTime=" + editTime +
                 ", orderId='" + orderId + '\'' +
                 ", maskName='" + maskName + '\'' +
                 ", maskNo='" + maskNo + '\'' +
+                ", imageUrl='" + imageUrl + '\'' +
                 '}';
     }
 }
