80364402ea38b7df893b68f34fbdda8449cddb9e
LZH <270377856@qq.com>
2020-06-19 21:15:56 +0800

    commit serviceImpl

--- src/main/java/edu/dgut/service/impl/RecordServiceImpl.java
+++ src/main/java/edu/dgut/service/impl/RecordServiceImpl.java
@@ -1,4 +1,39 @@
 package edu.dgut.service.impl;
 
-public class RecordServiceImpl {
+import edu.dgut.domain.Record;
+import edu.dgut.mapper.RecordMapper;
+import edu.dgut.service.RecordService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class RecordServiceImpl implements RecordService {
+    @Autowired
+    RecordMapper recordMapper;
+    @Override
+    public List<Record> getRecordList(int start, int limit, String aid, String uid, String status) {
+        return recordMapper.getRecordList(start,limit,aid,uid,status);
+    }
+
+    @Override
+    public Integer getRecordCount(String aid, String uid, String status) {
+        return recordMapper.getRecordCount(aid,uid,status);
+    }
+
+    @Override
+    public Record getRecordById(String aid) {
+        return recordMapper.getRecordById(aid);
+    }
+
+    @Override
+    public int updateRecord(Record record) {
+        return recordMapper.updateRecord(record);
+    }
+
+    @Override
+    public int deleteRecord(String aid) {
+        return recordMapper.deleteRecord(aid);
+    }
 }
