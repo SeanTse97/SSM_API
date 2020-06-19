8579f40ac2330830b5fb14d3f59e9715ee912145
LZH <270377856@qq.com>
2020-06-19 21:15:28 +0800

    commit service

--- src/main/java/edu/dgut/service/RecordService.java
+++ src/main/java/edu/dgut/service/RecordService.java
@@ -1,4 +1,19 @@
 package edu.dgut.service;
 
+import edu.dgut.domain.Record;
+import org.apache.ibatis.annotations.Param;
+
+import java.util.List;
+
 public interface RecordService {
+    //获取申请记录信息
+    public List<Record> getRecordList(int start, int limit, String aid,String uid,String status);
+
+    public Integer getRecordCount(String aid,String uid,String status);
+
+    public Record getRecordById(String aid);
+
+    public int updateRecord(Record record);
+
+    public int deleteRecord(String aid);
 }
