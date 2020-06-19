89fa5bf7fe0c0d4e99ed8f1898224af6210dbbe1
LZH <270377856@qq.com>
2020-06-19 21:14:49 +0800

    commit Mapper

--- src/main/java/edu/dgut/mapper/RecordMapper.java
+++ src/main/java/edu/dgut/mapper/RecordMapper.java
@@ -1,4 +1,22 @@
 package edu.dgut.mapper;
 
+import edu.dgut.domain.Mask;
+import edu.dgut.domain.Record;
+import org.apache.ibatis.annotations.Param;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+
+@Repository
 public interface RecordMapper {
+    public List<Record> getRecordList(@Param("start") int start, @Param("limit")int limit,
+                                      @Param("applyId") String aid,@Param("userId") String uid,@Param("applyStatus") String status);
+
+    public Integer getRecordCount(@Param("applyId") String aid,@Param("userId") String uid,@Param("applyStatus") String status);
+
+    public Record getRecordById(@Param("applyId") String aid);
+
+    public int updateRecord(Record record);
+
+    public int deleteRecord(String aid);
 }
