eb8ec85eb3ba3d7ef15b6b88d9bb847eeb2b62f7
super_xin <m13423197429@163.com>
2020-06-20 17:45:48 +0800

    添加判断条件

--- src/main/java/edu/dgut/controller/RecordController.java
+++ src/main/java/edu/dgut/controller/RecordController.java
@@ -60,26 +60,28 @@ public class RecordController {
         //获取未修改前的申请状态
         String oldStatus=record.getApplyStatus();
         //System.out.println(oldStatus);
-
         record.setEditTime(new Date().getTime());
         record.setApplyStatus(status);
-        int i= recordService.updateRecord(record);
         //申请状态只能由“待审核”变更至“通过”或“不通过”
         //当状态由“待审核”变更至“通过”时，库存容量才会减10
         if(oldStatus.equals("待审核")&&status.equals("通过")) {
+            int i= recordService.updateRecord(record);
             List<Mask> mkl = maskService.getMaskRecordList(0, 10, record.getMaskName());
             Mask u1 = mkl.get(0);
             //System.out.println(u1);
             maskService.updateMaskStore(record.getMaskName(), record.getEditTime(), -10, u1.getImageUrl());
             //此处更改库存的时间与record记录修改时间相同
-        }
-        if( i == 1){
-            map.put("success",Boolean.TRUE);
+            if( i == 1){
+                map.put("success",Boolean.TRUE);
+            }else{
+                map.put("success",Boolean.FALSE);
+                map.put("msg","服务端出错了！请稍候重试！！");
+            }
         }else{
             map.put("success",Boolean.FALSE);
-            map.put("msg","服务端出错了！请稍候重试！！");
+            map.put("msg","状态变化不支持！！");
         }
-        //map.put("success",Boolean.TRUE);
+
         return map;
     }
 
