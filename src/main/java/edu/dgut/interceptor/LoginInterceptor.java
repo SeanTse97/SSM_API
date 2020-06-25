f5542eedf6388f6112ffd68a3acdc8c255451f9f
super_xin <m13423197429@163.com>
2020-06-25 22:43:37 +0800

    添加拦截器类

--- src/main/java/edu/dgut/interceptor/LoginInterceptor.java
+++ src/main/java/edu/dgut/interceptor/LoginInterceptor.java
@@ -2,6 +2,7 @@ package edu.dgut.interceptor;
 
 import org.springframework.web.servlet.HandlerInterceptor;
 
+import javax.servlet.http.Cookie;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
@@ -10,18 +11,25 @@ public class LoginInterceptor implements HandlerInterceptor {
     public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler) throws Exception {
         //特殊的请求放行
         String requestURI = request.getRequestURI();
-        if (requestURI.equalsIgnoreCase("/login")){
+        if (requestURI.equalsIgnoreCase("/api/user/login") || requestURI.equalsIgnoreCase("/api/user/register")){
             return true;
         }
 
         //1.判断用户是不是登录？
-        String user = (String) request.getSession().getAttribute("user");
+        String user = null;
+        Cookie[] cookies  = request.getCookies();
+        for (Cookie c: cookies) {
+            if(c.getName().equals("user")){
+                user = c.getValue();
+                break;
+            }
+        }
         if (user!=null){
             //用户已经登录
             return true;
         }else {
             //3、如果没登录
-            request.getRequestDispatcher("/login").forward(request,response);
+            //request.getRequestDispatcher("/login").forward(request,response);
             return false;
         }
 
